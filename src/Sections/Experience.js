34aaed7d04b0daeadd847d2651468a46b4adf94d
heyyakash <akashsharma2002@gmail.com>
2022-07-12 22:25:11 +0530

    made more responsive

--- src/Sections/Experience.js
+++ src/Sections/Experience.js
@@ -7,21 +7,14 @@ import './Experience/experience.css';
 const Experience = () => {
   const {Experience} = data;
   return (
-    Experience&&
+    Experience &&
     <div className='exp-section-wrapper'>
       <section className='exp-section'>
         <img src={triangle} className = "triangle" alt="triangle" />
-        <img src={triangle} className = "triangle" alt="triangle" />
-        
-        
+        <img src={triangle} className = {`triangle ${Experience.length===1?"adjust-triangle":""}`} alt="triangle" />
         <h1 className='exp-section-heading'>Experience</h1>
-
         <div className='exp-container'>
-
           {Experience.map((data)=><ExperienceBox designation={data.Role} company = {data.CompanyName} description = {data.Description} duration = {data.Duration} />)}
-          
-          
-
         </div>
 
       </section>
