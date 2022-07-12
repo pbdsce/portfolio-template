0cb7c938ac59fb3fe453ab2bfc69ec27e2ac1c39
Rahulsharma4329 <rahulsharma4329@gmail.com>
2022-07-12 21:05:47 +0530

    added education component

--- src/Sections/Education.js
+++ src/Sections/Education.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import EducationComp from './Education/EducationComp';
+import data from "../data.json"
 
 const Education = () => {
+  const {Education} = data;
   return (
-    <div></div>
+    // Education&&
+    <>
+    {Education.map((data)=><EducationComp schlname={data.College} field = {data.Field} tenure={data.Duration}/>)}
+    </>
   )
 }
 
