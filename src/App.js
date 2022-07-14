fa1fd9a2c45fe074d323c1cea81a3db7de4767b5
anusha-shanbhog <anushapshanbhog@gmail.com>
2022-07-14 21:11:50 +0530

    Adding skills

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+
 import './App.css'
 import Header from './Sections/Header'
 import data from './data.json'
 import Education from './Sections/Education'
-import Skills from './Sections/Skills'
+import {Skills, SkillsContainer,SkillsImage, Container, SkillsTitle,  TitleComponent, Unit} from './Sections/Skills'
 import Experience from './Sections/Experience'
 import Projects from './Sections/Projects'
+import { Helmet } from "react-helmet"
+import {Diamond} from 'react-awesome-shapes'
+import styled from 'styled-components'
+import pythonLogo from './assets/pythonLogo.jpg';
 
-const SampleComponent = () => {
-    var Skills = data.Skills
-    return (
-      Skills.map((skill, index) => 
-          <h1 key={index}>{skill}</h1>
-      )
-    )
-}
+import javaLogo from './assets/javaLogo.jpg';
+import cLogo from './assets/cLogo.jpg';
+import cppLogo from './assets/cppLogo.png';
+import jsLogo from './assets/jsLogo.png';
+
 const App = () => {
-    var collegeName = data.Education[0].College
-    console.log('College Name: ', collegeName)
+   
     return (
         <>
-            <SampleComponent />
+         <div>
+      <Helmet bodyAttributes={{ style: "background-color : #23272A" }} />
+    
+    <div className="shape1">
+    <Diamond
+    color="linear-gradient(135deg, #00BA4D, #00BA4D)"
+    size="310px"
+    zIndex={0}
+/>
+</div>
+<div className="shape2">
+<Diamond
+    color="#23272A"
+    size="290px"
+    zIndex={0}
+/>
+
+</div>
+
+
+<div className="shape3">
+    <Diamond
+    color="linear-gradient(135deg, #00BA4D, #00BA4D)"
+    size="325px"
+    zIndex={0}
+/>
+</div>
+<div className="shape4">
+<Diamond
+    color="#23272A"
+    size="300px"
+    zIndex={0}
+/>
+
+</div>
+    </div>
+ 
+            
             <Header />
             <Education />
             <Skills />
--- src/Sections/Skills.js
+++ src/Sections/Skills.js
@@ -1,9 +1,139 @@
 import React from 'react'
+import styled from "styled-components";
+import data from '../data.json'
+import pythonLogo from '../assets/pythonLogo.jpg';
+import javaLogo from '../assets/javaLogo.jpg';
+import cLogo from '../assets/cLogo.jpg';
+import cppLogo from '../assets/cppLogo.png';
+import jsLogo from '../assets/jsLogo.png';
+import '../App.css'
+
 
 const Skills = () => {
   return (
-    <div></div>
-  )
+    <>
+    <div className="heading">Skills</div>
+    <div className="unit">
+      <Unit/>
+    </div>
+   </>
+
+  );
 }
 
-export default Skills
+const logosObject = {
+  'Javascript': "../assets/jsLogo.png",
+  'Python':"../assets/pythonLogo.jpg",
+ 'Java':"../assests/javaLogo.jpg",
+  'C':"../assets/cLogo.jpg"
+  }
+
+  
+
+const TitleComponent = () => {
+  const Skills = data.Skills
+  /*for(var i=0;i<logosObject.length; )
+  for(var j=0;i<logosObject.length; )
+  if(Skills[i]===logosObject[j])
+  {*/
+    return (
+      Skills.map(({skill,image} ,index)=> 
+        <Container>
+     <SkillsContainer>
+      <div img src="logosObject.image">
+
+      </div>
+      
+      </SkillsContainer>   
+     
+      <div className="title" key={index}>{skill}</div>
+     </Container>
+      
+    )
+    )
+
+  }
+
+ 
+
+
+const Unit=()=>
+{
+  styled.div`
+  height=8rem;
+width=100vw;
+position:absolute;
+padding:3%;
+margin-left:12%;
+float:center;
+justify-content:space-around;
+align-items:center;
+display:flex;
+
+  `;
+
+  return(       
+     <TitleComponent >{Skills}</TitleComponent>
+
+     )
+}
+
+
+const SkillsContainer=styled.div`
+
+
+display: flex;
+flex-direction=column;
+
+justify-content:space-around;
+align-items:center;
+width:7.5rem;;
+height:7.5rem;
+
+border-radius: 18px;
+
+backdrop-filter: blur(8px);
+-webkit-backdrop-filter: blur(9px);
+border: 1.6px solid rgba(255, 255, 255, 0.3);border-radius: 25px;
+backdrop-filter: blur(40px);
+
+box-shadow: 0 2px 10px rgb(0 0 0 / 10%), 0 10px 15px rgb(0 0 0 / 20%);
+
+background-image: linear-gradient(135deg, rgba(255,255,255,0.05), rgba(255,255,255,0.15));
+
+mix-blend-mode: hard-light;
+
+
+`;
+
+const SkillsImage=styled.img`
+height:6rem;
+width:6rem;
+
+`;
+
+const Container=styled.div`
+
+margin-top:5%;
+margin-left:4%;
+
+float:center;
+`;
+
+
+const SkillsTitle=styled.h3`
+  color: white;
+  font-size:1.2 rem;
+  text-align:match-parent;
+  font-style: italic;
+  font-family: Nunito;
+  
+`;
+
+export  {Skills}
+export  {SkillsContainer}
+export  {SkillsImage}
+export  {SkillsTitle}
+export  {Container}
+export  {TitleComponent}
+export  {Unit}
