0cc74a93eb6a5cc6a45b4b2cad102201e59123b0
heyyakash <akashsharma2002@gmail.com>
2022-07-14 15:09:01 +0530

    added components folder

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import Header from './Sections/Header'
 import data from './data.json'
 import Education from './Sections/Education'
 import Skills from './Sections/Skills'
-import Experience from './Sections/Experience'
+import Experience from './Components/Experience'
 import Projects from './Sections/Projects'
 
-// const SampleComponent = () => {
-//     var Skills = data.Skills
-//     return (
-//       Skills.map((skill, index) => 
-//           <h1 key={index}>{skill}</h1>
-//       )
-//     )
-// }
 const App = () => {
-    // var collegeName = data.Education[0].College
-    // console.log('College Name: ', collegeName)
+    var collegeName = data.Education[0].College
+    console.log('College Name: ', collegeName)
     return (
         <>
-            {/* <SampleComponent />
             <Header />
             <Education />
-            <Skills /> */}
+            <Skills />
             <Experience />
-            {/* <Projects /> */}
+            <Projects />
         </>
     )
 }
