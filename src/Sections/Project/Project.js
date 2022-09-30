9a739f43cf0b6eaa0fc5172ea5c9b6177bdd6fa4
TheGreatFellow <shreyas42000@gmail.com>
2022-10-01 01:56:16 +0530

    minor modifications in projects section

--- src/Sections/Project/Project.js
+++ src/Sections/Project/Project.js
@@ -1,21 +1,16 @@
 import './Project.css'
-const Project = ({data}) => {
-    return(
+const Project = ({ data }) => {
+    return (
         <>
-        
-            <div className  = 'card'>
+            <div className='card'>
                 <div className='image-icon'>
-                    <img src={data.ImageLink} alt={data.Title}/>
+                    <img src={data.ImageLink} alt={data.Title} />
                 </div>
-                <label className='title'>
-                    {data.Title}
-                </label>
-                <p className='desc'>
-                    {data.Description}
-                </p>
+                <label className='title'>{data.Title}</label>
+                <p className='desc'>{data.Description}</p>
             </div>
         </>
-    );
-};
+    )
+}
 
-export default Project;
+export default Project
--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -4,29 +4,39 @@ import data from '../data.json'
 import rectangle from '../assets/rectangle.png'
 import './Project/Project.css'
 const Projects = () => {
-  const projects = data.Projects;
-  return (
-    <>
-    { projects &&  
-    
-    <>
-          <div className='Project-Title'>
-          
-          <h2>Projects</h2>
-          <div className="projects">
-
-          <img src={rectangle} alt="rectangle" className = "rectangle" />
-          <img src={rectangle} className = {`rectangle ${projects.length===1?"adjust-rectangle":""}`} alt="rectangle" />
-          </div>
-          <div className='flex-row'>
-              {projects.map((data) => <Project data = {data} />)}
-          </div>
-          </div>
-    </>
-    }
-    
-    </>
-  )
+    const projects = data.Projects
+    return (
+        <>
+            {projects && (
+                <div className='project-wrapper'>
+                    <div className='Project-Title'>
+                        <h2>Projects</h2>
+                        <div className='projects'>
+                            <img
+                                src={rectangle}
+                                alt='rectangle'
+                                className='rectangle'
+                            />
+                            <img
+                                src={rectangle}
+                                className={`rectangle ${
+                                    projects.length === 1
+                                        ? 'adjust-rectangle'
+                                        : ''
+                                }`}
+                                alt='rectangle'
+                            />
+                        </div>
+                        <div className='flex-row'>
+                            {projects.map((data) => (
+                                <Project data={data} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
 }
 
 export default Projects
